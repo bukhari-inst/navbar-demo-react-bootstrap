ad19b0018b52a7b27c541d1a715964debbe96c1f
Bukhori Inst <58775021+bukhari-inst@users.noreply.github.com>
2021-09-01 10:43:32 +0700

    update all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'assets/index.scss';
+import 'assets/scss/style.scss';
+import NavbarComp from './components/NavbarComp';
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <NavbarComp />
+    </div>
+  );
 }
 
 export default App;
